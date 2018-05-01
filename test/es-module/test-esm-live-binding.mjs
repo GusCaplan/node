fd4d955b19c99cf92a644574c2f7909c2bdd7666
Gus Caplan <me@gus.host>
2018-05-01 13:17:27 -0500

    add tests for setter behaviour

--- test/es-module/test-esm-live-binding.mjs
+++ test/es-module/test-esm-live-binding.mjs
@@ -4,6 +4,8 @@ import '../common';
 import assert from 'assert';
 
 import fs, { readFile } from 'fs';
+import events from 'events';
+import util from 'util';
 
 const s = Symbol();
 const fn = () => s;
@@ -40,3 +42,58 @@ Reflect.defineProperty(fs, 'readFile', {
 
 assert.strictEqual(fs.readFile(), s);
 assert.strictEqual(readFile(), s);
+
+assert.throws(() => {
+  Object.defineProperty(events, 'defaultMaxListeners', { value: 3 });
+}, {});
+
+// keep these ones last because they mess with prototypes
+
+let count = 0;
+Reflect.defineProperty(Function.prototype, 'defaultMaxListeners', {
+  configurable: true,
+  enumerable: true,
+  get: function() { return ++count; },
+  set: function(v) {
+    Reflect.defineProperty(this, 'defaultMaxListeners', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: v,
+    });
+  },
+});
+
+assert.strictEqual(10, events.defaultMaxListeners);
+assert.strictEqual(11, ++events.defaultMaxListeners);
+
+assert.strictEqual(1, Function.prototype.defaultMaxListeners);
+assert.strictEqual(2, Function.prototype.defaultMaxListeners);
+Function.prototype.defaultMaxListeners = 'foo';
+assert.strictEqual('foo', Function.prototype.defaultMaxListeners);
+
+assert.strictEqual(11, events.defaultMaxListeners);
+
+count = 0;
+const p = {
+  get foo() { return ++count; },
+  set foo(v) {
+    Reflect.defineProperty(this, 'foo', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: v,
+    });
+  },
+};
+
+util.__proto__ = p; // eslint-disable-line no-proto
+
+assert.strictEqual(1, util.foo);
+util.foo = 'bar';
+assert.strictEqual(1, count);
+assert.strictEqual('bar', util.foo);
+assert.strictEqual(2, p.foo);
+assert.strictEqual(3, p.foo);
+p.foo = 'foo';
+assert.strictEqual('foo', p.foo);
