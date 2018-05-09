99234088d29f02d6f833fb05ba721d47eee54c41
John-David Dalton <john.david.dalton@gmail.com>
2018-05-09 09:38:18 -0700

    Remove try-caches from live binding tests.

--- test/es-module/test-esm-live-binding.mjs
+++ test/es-module/test-esm-live-binding.mjs
@@ -7,134 +7,109 @@ import fs, { readFile } from 'fs';
 import events from 'events';
 import util from 'util';
 
-let error;
-let threw = false;
-
 const fsDescriptor = Reflect.getOwnPropertyDescriptor(fs, 'readFile');
 
-try {
-  const s = Symbol();
-  const fn = () => s;
+const s = Symbol();
+const fn = () => s;
 
-  Reflect.deleteProperty(fs, 'readFile');
+Reflect.deleteProperty(fs, 'readFile');
 
-  assert.strictEqual(fs.readFile, undefined);
-  assert.strictEqual(readFile, undefined);
+assert.strictEqual(fs.readFile, undefined);
+assert.strictEqual(readFile, undefined);
 
-  fs.readFile = fn;
+fs.readFile = fn;
 
-  assert.strictEqual(fs.readFile(), s);
-  assert.strictEqual(readFile(), s);
+assert.strictEqual(fs.readFile(), s);
+assert.strictEqual(readFile(), s);
 
-  Reflect.deleteProperty(fs, 'readFile');
-  Reflect.defineProperty(fs, 'readFile', {
-    value: fn,
-    configurable: true,
-    writable: true,
-  });
+Reflect.defineProperty(fs, 'readFile', {
+  value: fn,
+  configurable: true,
+  writable: true,
+});
 
-  assert.strictEqual(fs.readFile(), s);
-  assert.strictEqual(readFile(), s);
+assert.strictEqual(fs.readFile(), s);
+assert.strictEqual(readFile(), s);
 
-  Reflect.deleteProperty(fs, 'readFile');
+Reflect.deleteProperty(fs, 'readFile');
 
-  assert.strictEqual(fs.readFile, undefined);
-  assert.strictEqual(readFile, undefined);
+assert.strictEqual(fs.readFile, undefined);
+assert.strictEqual(readFile, undefined);
 
-  Reflect.defineProperty(fs, 'readFile', {
-    get() { return fn; },
-    set() {},
-    configurable: true,
-  });
+Reflect.defineProperty(fs, 'readFile', {
+  get() { return fn; },
+  set() {},
+  configurable: true,
+});
 
-  assert.strictEqual(fs.readFile(), s);
-  assert.strictEqual(readFile(), s);
-} catch (e) {
-  threw = true;
-  error = e;
-}
+assert.strictEqual(fs.readFile(), s);
+assert.strictEqual(readFile(), s);
 
 Reflect.defineProperty(fs, 'readFile', fsDescriptor);
-
-if (threw) {
-  throw error;
-}
-
-error = undefined;
-threw = false;
 
 const originDefaultMaxListeners = events.defaultMaxListeners;
 const utilProto = util.__proto__; // eslint-disable-line no-proto
 
-try {
-  let count = 0;
+let count = 0;
 
-  Reflect.defineProperty(Function.prototype, 'defaultMaxListeners', {
-    configurable: true,
-    enumerable: true,
-    get: function() { return ++count; },
-    set: function(v) {
-      Reflect.defineProperty(this, 'defaultMaxListeners', {
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        value: v,
-      });
-    },
-  });
+Reflect.defineProperty(Function.prototype, 'defaultMaxListeners', {
+  configurable: true,
+  enumerable: true,
+  get: function() { return ++count; },
+  set: function(v) {
+    Reflect.defineProperty(this, 'defaultMaxListeners', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: v,
+    });
+  },
+});
 
-  assert.strictEqual(events.defaultMaxListeners, originDefaultMaxListeners);
-  assert.strictEqual(++events.defaultMaxListeners,
-                     originDefaultMaxListeners + 1);
+assert.strictEqual(events.defaultMaxListeners, originDefaultMaxListeners);
+assert.strictEqual(++events.defaultMaxListeners,
+                   originDefaultMaxListeners + 1);
 
-  assert.strictEqual(Function.prototype.defaultMaxListeners, 1);
+assert.strictEqual(Function.prototype.defaultMaxListeners, 1);
 
-  Function.prototype.defaultMaxListeners = 'foo';
+Function.prototype.defaultMaxListeners = 'foo';
 
-  assert.strictEqual(Function.prototype.defaultMaxListeners, 'foo');
-  assert.strictEqual(events.defaultMaxListeners, originDefaultMaxListeners + 1);
+assert.strictEqual(Function.prototype.defaultMaxListeners, 'foo');
+assert.strictEqual(events.defaultMaxListeners, originDefaultMaxListeners + 1);
 
-  count = 0;
+count = 0;
 
-  const p = {
-    get foo() { return ++count; },
-    set foo(v) {
-      Reflect.defineProperty(this, 'foo', {
-        configurable: true,
-        enumerable: true,
-        writable: true,
-        value: v,
-      });
-    },
-  };
+const p = {
+  get foo() { return ++count; },
+  set foo(v) {
+    Reflect.defineProperty(this, 'foo', {
+      configurable: true,
+      enumerable: true,
+      writable: true,
+      value: v,
+    });
+  },
+};
 
-  util.__proto__ = p; // eslint-disable-line no-proto
+util.__proto__ = p; // eslint-disable-line no-proto
 
-  assert.strictEqual(util.foo, 1);
+assert.strictEqual(util.foo, 1);
 
-  util.foo = 'bar';
+util.foo = 'bar';
 
-  assert.strictEqual(count, 1);
-  assert.strictEqual(util.foo, 'bar');
-  assert.strictEqual(p.foo, 2);
+assert.strictEqual(count, 1);
+assert.strictEqual(util.foo, 'bar');
+assert.strictEqual(p.foo, 2);
 
-  p.foo = 'foo';
+p.foo = 'foo';
 
-  assert.strictEqual(p.foo, 'foo');
-} catch (e) {
-  threw = true;
-  error = e;
-}
+assert.strictEqual(p.foo, 'foo');
 
 events.defaultMaxListeners = originDefaultMaxListeners;
 util.__proto__ = utilProto; // eslint-disable-line no-proto
 
 Reflect.deleteProperty(util, 'foo');
 Reflect.deleteProperty(Function.prototype, 'defaultMaxListeners');
-
-if (threw) {
-  throw error;
-}
 
 assert.throws(
   () => Object.defineProperty(events, 'defaultMaxListeners', { value: 3 }),
